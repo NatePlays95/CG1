4629609e76d9640c451c6ee7f63b72a162a23d90
Natan Maia <natanmaiaplays@outlook.com>
2023-10-02 19:09:32 -0300

    a

--- main.cpp
+++ main.cpp
@@ -11,11 +11,12 @@
 
 using namespace std;
 
-int WINDOW_WIDTH = 800; int WINDOW_HEIGHT = 600;
+// int WINDOW_WIDTH = 800; int WINDOW_HEIGHT = 600;
+int WINDOW_WIDTH = 500; int WINDOW_HEIGHT = 500;
 SDL_Color BACKGROUND_COLOR = {100,100,100,255};
-Vec3 AMBIENT_LIGHT = Vec3(0.2,0.2,0.3);
+// Vec3 AMBIENT_LIGHT = Vec3(0.2,0.2,0.3);
+Vec3 AMBIENT_LIGHT = Vec3(0.3,0.3,0.3);
 
-int NLIN = 60; int NCOL = 80;
 
 
 SDL_Color renderLightingStep(Ray& raycast, std::list<Shape*>& renderObjectsList, std::list<Light*>& renderLightsList) {
@@ -31,6 +32,7 @@ SDL_Color renderLightingStep(Ray& raycast, std::list<Shape*>& renderObjectsList,
         Vec3 i = light->intensity;
         Vec3 matr = raycast.contact_material.roughness;
         Vec3 mats = raycast.contact_material.specular;
+        double matm = raycast.contact_material.hardness;
 
         //test for shadows
         Ray shadowRaycast = Ray(light->position, l * (-1));
@@ -60,7 +62,7 @@ SDL_Color renderLightingStep(Ray& raycast, std::list<Shape*>& renderObjectsList,
         Vec3 specIAtK = Vec3(i.x*mats.x, i.y*mats.y, i.z*mats.z);
         Vec3 r = l.reflect(n);
         Vec3 v = raycast.direction;
-        double mExp = 100; //hardness
+        double mExp = matm; //hardness
         double specFactor = pow(max(0.0, r.dot(v)),mExp);
         Vec3 specularIntensity = specIAtK * specFactor;
         specularIntensity = specularIntensity.clampedPositive();
@@ -78,7 +80,8 @@ SDL_Color renderLightingStep(Ray& raycast, std::list<Shape*>& renderObjectsList,
 void render(SDL_Renderer * renderer, Camera camera, std::list<Shape*>& renderObjectsList, std::list<Light*>& renderLightsList){
     //instruções da tarefa
 
-    const int nLin = 60; const int nCol = 80;
+    // const int nLin = 60; const int nCol = 80;
+    const int nLin = 500; const int nCol = 500;
     SDL_Color canvas[nLin][nCol];    
 
     double dx = camera.frameWidth/nCol;
@@ -128,24 +131,27 @@ void render(SDL_Renderer * renderer, Camera camera, std::list<Shape*>& renderObj
 int main(int argv, char** args)
 {
 
-    Mat4 matI = Mat4::scaleMatrix(1,1,1);
-    Mat4 matRandom = Mat4();
-    matRandom.mat = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
-    Mat4 matTest = matRandom * matI;
+    // Mat4 matI = Mat4::scaleMatrix(1,1,1);
+    // Mat4 matRandom = Mat4();
+    // matRandom.mat = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
+    // Mat4 matTest = matRandom * matI;
 
-    Vec4 coords = Vec4(Vec3(2, 1.5, 0.5), 1);
-    Mat4 matScale = Mat4::scaleMatrix(2,1.5,0.5);
-    Vec4 scaledCoords = matScale * coords;
+    // Vec4 coords = Vec4(Vec3(2, 1.5, 0.5), 1);
+    // Mat4 matScale = Mat4::scaleMatrix(2,1.5,0.5);
+    // Vec4 scaledCoords = matScale * coords;
 
     Camera camera;
-    camera.frameWidth = 8;
-    camera.frameHeight = 6;
-    camera.frameDistance = 6;
+    // camera.frameWidth = 8;
+    // camera.frameHeight = 6;
+    // camera.frameDistance = 6;
+    camera.frameWidth = 60;
+    camera.frameHeight = 60;
+    camera.frameDistance = 30;
                     //position   //direction
-    camera.eye = Ray(Vec3(0,0,0), Vec3(0,0,-1));
+    camera.eye = Ray(Vec3(0,0,-30), Vec3(0,0,-1));
 
 
-    PointLight* pointLight = new PointLight(Vec3(20, 30, 0), Vec3(0.7,0.7,0.7));
+    PointLight* pointLight = new PointLight(Vec3(0, 60, -30), Vec3(0.7,0.7,0.7));
     // PointLight* pointLight2 = new PointLight(Vec3(10, 20, 0), Vec3(1,1,1));
 
     std::list<Light*> renderLightsList;
@@ -155,17 +161,15 @@ int main(int argv, char** args)
 
 
 
-    Sphere* sphere = new Sphere(Vec3(0,-2,-15), 3);
-    sphere->color = {255, 255, 0, 255};
-    // sphere->material = Material(Vec3(0.5,0.5,0), Vec3(1,0,0));
-    sphere->material = Material(Vec3(0.5,0.5,0.5), Vec3(1,1,0.7));
 
-    Sphere* sphere2 = new Sphere(Vec3(4,0,-22), 4);
-    sphere2->color = {255, 0, 0, 255};
-    sphere2->material = Material(Vec3(1,0.5,1), Vec3(0,0,0.5));
+    Sphere* sphere3 = new Sphere(Vec3(0,0,-100), 40);
+    sphere3->material = Material(Vec3(0.7,0.2,0.2), Vec3(0.7,0.2,0.2), 10);
 
-    Plane* plane = new Plane(Vec3(0,-3,0), Vec3(0,1,0));
-    plane->material = Material(Vec3(0.5,0.5,0.5), Vec3(1,1,1));
+    Plane* ground = new Plane(Vec3(0,-40,0), Vec3(0,1,0));
+    ground->material = Material(Vec3(0.2, 0.7, 0.2), Vec3(0,0,0));
+
+    Plane* wall = new Plane(Vec3(0,0,-200), Vec3(0,0,1));
+    wall->material = Material(Vec3(0.3, 0.3, 0.7), Vec3(0,0,0));
 
     // Disk* disk = new Disk(Vec3(20,-10,-20), Vec3(-1,0,0), 20);
     // disk->color = {200, 200, 255, 255};
@@ -177,9 +181,9 @@ int main(int argv, char** args)
     // cone->setColor({255, 255, 100, 255});
 
     std::list<Shape*> renderObjectsList;
-    renderObjectsList.push_front(sphere);
-    renderObjectsList.push_front(sphere2);
-    renderObjectsList.push_front(plane);
+    renderObjectsList.push_front(sphere3);
+    renderObjectsList.push_front(ground);
+    renderObjectsList.push_front(wall);
     // renderObjectsList.push_front(plane);
     // renderObjectsList.push_front(disk);
     // renderObjectsList.push_front(cylinder);
@@ -225,14 +229,14 @@ int main(int argv, char** args)
                         camera.eye.position.x += 0.5;
                         break;
 
-                    case SDLK_q:
-                        NLIN /= 10;
-                        NCOL /= 10;
-                        break;
-                    case SDLK_e:
-                        NLIN *= 10;
-                        NCOL *= 10;
-                        break;
+                    // case SDLK_q:
+                    //     NLIN /= 10;
+                    //     NCOL /= 10;
+                    //     break;
+                    // case SDLK_e:
+                    //     NLIN *= 10;
+                    //     NCOL *= 10;
+                    //     break;
                     // Add more cases for other keys if needed
                 }
             }
--- source/lighting/material.h
+++ source/lighting/material.h
@@ -7,8 +7,9 @@ class Material {
     public:
         Vec3 specular;
         Vec3 roughness;
+        double hardness;
 
-        Material(Vec3 specular_in = Vec3(1,1,1), Vec3 roughness_in = Vec3(1,1,1));
+        Material(Vec3 specular_in = Vec3(1,1,1), Vec3 roughness_in = Vec3(1,1,1), double hardness_in = 1.0);
 };
 
 #endif
